62f54a2541fab832c7f661b39f336510e0d08f55
Dagny-Rebhan <drebhan@buffaloseminary.org>
2018-08-03 16:11:19 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,3 +1,10 @@
+ENV['SINATRA_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+require_relative "models/model.rb"
+
 class ApplicationController < Sinatra::Base
 
   configure do
